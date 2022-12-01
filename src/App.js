c7037648cad8694e6b4a28f094e75ab1e68a9a4a
ethiobek <arkishasoftware@gmail.com>
2022-12-01 13:37:42 +0400

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "Bereket Zergaw",
+      age: 12,
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            I'm <code>{this.state.name}</code>. I am {this.state.age} years old
+            !
+          </p>
+          <button
+            onClick={() => {
+              this.setState({ name: "Btnu" });
+            }}
+          >
+            Learn React
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
